5c6c0fe60c460fee36034909cd405b358a020454
BrenoBar51 <brenobarcelos9@gmail.com>
2023-06-26 23:27:49 -0300

    testes falhos

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('btn')
 const teste = document.getElementById('teste')
+const btn2 = document.getElementById('btn2')
+const teste2 = document.getElementById('teste2')
 
 async function kanyeWest(){
     const response = await fetch('https://api.kanye.rest')
@@ -8,5 +10,12 @@ async function kanyeWest(){
     teste.innerHTML = data.quote
 }
 
-btn.addEventListener('click', kanyeWest)
+async function chuckNorris(){
+    const response = await fetch('https://api.chucknorris.io/jokes/random')
+    const data = await response.json();
+    console.log(data.quote)
+    teste2.innerHTML = data.quote
+}
 
+btn.addEventListener('click', kanyeWest)
+btn2.addEventListener('click', chuckNorris)
